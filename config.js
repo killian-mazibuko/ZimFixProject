70c1d872469d1e556db80f3b5c438816863947ff
killian-mazibuko <mzbkil001@myuct.ac.za>
2018-04-19 20:42:47 +0200

    Initial Commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://zimfixdb:QqbZq4Vescbe2E1YgHdr7492QJNGJfOdo4SiE4cjeXXoEX4W32XKdisnQamW4VjwooH1vFjw9dpQdManLYbHqA==@zimfixdb.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+}
